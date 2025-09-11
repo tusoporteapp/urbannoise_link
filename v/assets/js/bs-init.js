7d91d94a95a031bb8dc1de9f497c69c223a61a2d
TuSoporteApp <84728004+tusoporteapp@users.noreply.github.com>
2025-09-11 16:27:10 -0500

    UPDATE

--- v/assets/js/bs-init.js
+++ v/assets/js/bs-init.js
@@ -1,5 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+	var toastTriggers = document.querySelectorAll('[data-bs-toggle="toast"]');
+
+	for (let toastTrigger of toastTriggers) {
+		toastTrigger.addEventListener('click', function () {
+			var toastSelector = toastTrigger.getAttribute('data-bs-target');
+
+			if (!toastSelector) return;
+
+			try {
+				var toastEl = document.querySelector(toastSelector);
+
+				if (!toastEl) return;
+
+				var toast = new bootstrap.Toast(toastEl);
+				toast.show();
+			}
+			catch(e) {
+				console.error(e);
+			}
+		})
+	}
+
 	var products = document.querySelectorAll('[data-bss-dynamic-product]');
 
 	for (var product of products) {
