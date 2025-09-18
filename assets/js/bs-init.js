b122fa3409e2190081a3952bf6dff784e2deb513
TuSoporteApp <84728004+tusoporteapp@users.noreply.github.com>
2025-09-18 09:43:20 -0500

    update

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,3 +1,14 @@
+
+if (window.innerWidth < 768) {
+	[].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach(function (elem) {
+		elem.classList.remove('animated');
+		elem.removeAttribute('data-bss-hover-animate');
+		elem.removeAttribute('data-aos');
+		elem.removeAttribute('data-bss-parallax-bg');
+		elem.removeAttribute('data-bss-scroll-zoom');
+	});
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 
 	var toastTriggers = document.querySelectorAll('[data-bs-toggle="toast"]');
